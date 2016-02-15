147671faa0c2947809b98e0603ddbff890e8df34
Yasunari Flores <carlosyasu91@gmail.com>
2016-02-15 15:04:01 -0800

    Added item structure and login mock functionallity

--- client/app/app.js
+++ client/app/app.js
@@ -9,10 +9,34 @@ var app = angular.module('shopApp', [
     controller: 'loginController',
     templateUrl: 'app/login/login.html'
   })
+  .when('/dashboard', {
+    controller: 'dashboardController',
+    templateUrl: 'app/dashboard/dashboard.html'
+  })
+  .when('/create', {
+    controller: 'createController',
+    templateUrl: 'app/create/create.html'
+  })
+  .otherwise({
+    redirectTo: '/login'
+  });
 })
-.controller('loginController', function($scope){
+.controller('loginController', function($scope, $location){
   $scope.test = 'Hello';
   $scope.login = function(){
-
+    //check username and password
+    if($scope.username === USERNAME && $scope.password === PASSWORD){
+      //redirect to dashboard
+      $location.path('/dashboard');
+    } else {
+      //redirect to login
+      $location.path('/login');
+    }
   };
+})
+.controller('dashboardController', function($scope){
+
+})
+.controller('createController', function($scope){
+
 });
