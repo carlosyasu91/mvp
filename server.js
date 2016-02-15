3be4854fcc0d772a4ad5ab1223cad1844ff1c560
Yasunari Flores <carlosyasu91@gmail.com>
2016-02-15 12:06:07 -0800

    Add styles file, add .bowerrc file, added angular to the index

--- server.js
+++ server.js
@@ -5,20 +5,26 @@ var app = express();
 
 var port = 8000;
 
-
-
 app.set('views', __dirname + '/views');
-
 
 app.get('/', function(req, res){
   //read index file
-  fs.readFile('public/login.html', function(err, data){
+  fs.readFile('public/dashboard.html', function(err, data){
     if(err) throw err;
     //set head to html and send the index
     res.setHeader('Content-Type', 'text/html');
     res.send(data);
   });
 });
+
+app.get('/login', function(req, res){
+  fs.readFile('public/login.html', function(err, data){
+    if(err) throw err;
+    //set head to html and send the index
+    res.setHeader('Content-Type', 'text/html');
+    res.send(data);
+  });
+})
 
 app.post('/', function(req, res){
 });
